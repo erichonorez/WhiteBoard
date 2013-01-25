bde1ead34b255049cb2817ecd25ae840a7c9dec0
Eric Honorez <eric.honorez@gmail.com>
2013-01-25 06:52:48 +0100

    Added: WebSocket communications

---
+++ WhiteBoardServer.js
@@ -0,0 +1,128 @@
+// http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
+"use strict";
+
+// Optional. You will see this name in eg. 'ps' or 'top' command
+process.title = 'WhiteBoad';
+
+// Port where we'll run the websocket server
+var webSocketsServerPort = 1337;
+
+// websocket and http servers
+var webSocketServer = require('websocket').server;
+var http = require('http');
+
+/**
+ * Global variables
+ */
+// latest 100 messages
+var messagesHistory = [];
+var linesHistory = [];
+// list of currently connected clients (users)
+var clients = [];
+
+/**
+ * Helper function for escaping input strings
+ */
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+/**
+ * HTTP server
+ */
+var server = http.createServer(function (request, response) {
+    // Not important for us. We're writing WebSocket server, not HTTP server
+});
+server.listen(webSocketsServerPort, function () {
+    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+});
+
+/**
+ * WebSocket server
+ */
+var wsServer = new webSocketServer({
+    // WebSocket server is tied to a HTTP server. WebSocket request is just
+    // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
+    httpServer: server
+});
+
+// This callback function is called every time someone
+// tries to connect to the WebSocket server
+wsServer.on('request', function (request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+
+    // accept connection - you should check 'request.origin' to make sure that
+    // client is connecting from your website
+    // (http://en.wikipedia.org/wiki/Same_origin_policy)
+    var connection = request.accept(null, request.origin);
+    // we need to know client index to remove them on 'close' event
+    var index = clients.push(connection) - 1;
+
+    console.log((new Date()) + ' Connection accepted.');
+
+    // send back chat history
+    if (linesHistory.length > 0) {
+        console.log("Sending coordinates history...");
+        connection.sendUTF(JSON.stringify({ type: 'linesHistory', data: linesHistory }));
+    }
+    if (messagesHistory.length > 0) {
+        console.log("Sending messages history...");
+        connection.sendUTF(JSON.stringify({ type: 'messagesHistory', data: messagesHistory }));
+    }
+
+    // user sent some message
+    connection.on('message', function (message) {
+        if (message.type === 'utf8') { // accept only text
+            try {
+                var received = JSON.parse(message.utf8Data);
+                switch (received.type) {
+                    case "coordinates":
+                        console.log(new Date() + "Received Coordinates: " + received)
+                        //save in the history
+                        linesHistory.push(received);
+                        //broadcast message
+                        for (var i = 0; i < clients.length; i++) {
+                            if (index != i) {
+                                clients[i].sendUTF(message.utf8Data);
+                            }
+                        }
+                        break;
+                    case "message":
+                        console.log((new Date()) + ' Received Message: ' + received);
+                        // we want to keep history of all sent messages
+                        messagesHistory.push(received);
+                        messagesHistory = messagesHistory.slice(-100);
+
+                        // broadcast message to all connected clients
+                        for (var i = 0; i < clients.length; i++) {
+                            clients[i].sendUTF(message.utf8Data);
+                        }
+                        break;
+                    case "activity":
+                        console.log("Activity: " + received.user + " is " + received.action);
+                        for (var i = 0; i < clients.length; i++) {
+                            clients[i].sendUTF(message.utf8Data);
+                        }
+                        break;
+                    case "command":
+                        console.log("Command: " + received.action);
+                        if (received.action == "clean") {
+                            linesHistory = [];
+                        }
+                        for (var i = 0; i < clients.length; i++) {
+                            clients[i].sendUTF(message.utf8Data);
+                        }
+                        break;
+                }
+            } catch (exception) {
+                console.log(exception);
+            }
+        }
+    });
+
+    // user disconnected
+    connection.on('close', function (connection) {
+        clients.splice(index, 1);
+    });
+
+});
--- js/whiteboard.js
+++ js/whiteboard.js
@@ -1,8 +1,15 @@
-//Globals variables
-var canvas, context, socket, coordinates, username = null;
-//Helpers
+/**
+ * Globals variables
+ */
+var canvas, context, socket, username, viewModel = null;
+var coordinates = "";
+var url = "ws://localhost:1337/";
+var isDrawing = false;
+/**
+ * Helpers functions
+ */
 function formatDate(date) {
-    twoDigits(date.getHours()) +
+   return twoDigits(date.getHours()) +
     ":" + twoDigits(date.getMinutes()) +
     ":" + twoDigits(date.getSeconds());
 }
@@ -12,39 +19,187 @@ function twoDigits(value) {
     }
     return value;
 }
-//Classes
-var WhiteBoardViewModel = function() {
-    this.messages = ko.observableArray([
-        { time: function () { return formatDate(new Date) }, user: "Eric", text: "Hello, world" }
-    ]);
+/**
+ * Classes
+ */
+//ViewModel to handle the application
+var WhiteBoardViewModel = function () {
+    this.messages = ko.observableArray();
     this.username = ko.observable();
     this.setUsername = function () {
         username = this.username();
         console.log("Username selected: " + username);
         console.log("Initializing the connection...");
-        //Open the socket
+        socket = initWebSocket();
         console.log("Conntection initialized.");
     };
+};
+//Application WebSocket
+WebSocket.prototype.sendJSON = function (obj) {
+    return this.send(JSON.stringify(obj));
+};
+WebSocket.prototype.sendMessage = function (message) {
+    this.sendJSON(message);
+};
+WebSocket.prototype.sendActivity = function (activity) {
+    this.sendJSON(activity);
+};
+WebSocket.prototype.sendLine = function (line) {
+    this.sendJSON(line);
+};
+WebSocket.prototype.sendCommand = function (command) {
+    this.sendJSON(command);
+};
+//Models
+//Protocol Message
+function Message(user, text) {
+    this.type = "message";
+    this.user = user;
+    this.text = text;
+    this.time = Date.now();
+}
+Message.prototype.formattedDate = function () {
+    return formatDate(new Date(this.time));
+};
+//Protocol Activity
+function Activity(user, action) {
+    this.type = "activity";
+    this.user = user;
+    this.action = action;
+}
+//Protocol Coordinates
+function Coordinate(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Coordinate.prototype.toString = function () {
+    return this.x + "#" + this.y;
+};
+//Protocol Line
+function Line(coordinates, context) {
+    this.type = "coordinates";
+    this.coordinates = coordinates;
+    this.context = {
+        strokeStyle: context.strokeStyle,
+        strokeWidth: context.strokeWidth
+    };
+}
+//Protocol Command
+function Command(action) {
+    this.type = "command";
+    this.action = action;
+}
+/**
+ * FACTORIES
+ */
+function MessageFactory() {};
+MessageFactory.prototype.fromJSON = function(json) {
+    var message = new Message(json.user, json.text);
+    message.time = json.time;
+    return message;
+};
+function initWebSocket() {
+    socket = new WebSocket(url);
+    socket.addEventListener("open", function (event) { /* do something when connected */ });
+
+    socket.addEventListener("message", function (event) {
+        var data = JSON.parse(event.data);
+        if (null == data.type) { return; }
+        if (data.type == "messagesHistory") {
+            var factory = new MessageFactory();
+            for (var i = 0; i < data.data.length; i++) {
+                viewModel.messages.push(factory.fromJSON(data.data[i]));
+            }
+        } else if (data.type == "message") {
+            var factory = new MessageFactory();
+            viewModel.messages.push(factory.fromJSON(data));
+        } else if (data.type == "coordinates") {
+            $("#activity").html("");
+            context.beginPath();
+            var coordinates = data.coordinates.split(",");
+            var firstCoordinates = coordinates[0].split("#");
+            context.moveTo(firstCoordinates[0], firstCoordinates[1]);
+            for (var i = 1; i < coordinates.length; i++) {
+                var values = coordinates[i].split("#");
+                context.lineTo(values[0], values[1]);
+                context.stroke();
+            }
+        } else if (data.type == "linesHistory") {
+            for (var i = 0; i < data.data.length; i++) {
+                context.beginPath();
+                var coordinates = data.data[i].coordinates.split(",");
+                var firstCoordinates = coordinates[0].split("#");
+                context.moveTo(firstCoordinates[0], firstCoordinates[1]);
+                for (var i = 1; i < coordinates.length; i++) {
+                    var values = coordinates[i].split("#");
+                    context.lineTo(values[0], values[1]);
+                    context.stroke();
+                }
+            }
+        } else if (data.type == "activity") {
+            $("#activity").html(data.user + " is " + data.action + "...");
+        } else if (data.type == "command") {
+            if (data.action == "clean") {
+                context.clearRect(0, 0, canvas.width, canvas.height);
+            }
+        }
+    });
+    socket.addEventListener("error", function (event) { /* do something on error */});
+    socket.addEventListener("close", function (event) { /* do something on close */});
+    return socket;
 };
 
 $(document).ready(function () {
     //configure styles
-    $("#messages").scrollTop($("#messages").height());
-
-    //configure canvas style
-    var canvasContainer = $("#board");
-    var parentCanvasContainer = canvasContainer.parent();
-    canvasContainer.width = parentCanvasContainer.width;
-    canvasContainer.height = parentCanvasContainer.height;
+    //$("#messages").scrollTop($("#messages").height());
 
     //initialize canvas and its context
     canvas = window.document.getElementById("board");
     context = canvas.getContext("2d");
 
     //event binding
-    
-    
-
+    var canvasElement = $("#board");
+    canvasElement.mousedown(function (event) {
+        isDrawing = true;
+        context.beginPath();
+        socket.sendActivity(new Activity(username, "drawing"));
+    });
+    //on mouse move collect data
+    canvasElement.mousemove(function (event) {
+        if (!isDrawing) {
+            return;
+        }
+        var x = event.clientX - $(this).offset().left;
+        var y = event.clientY - $(this).offset().top;
+        context.lineTo(x, y);
+        context.stroke();
+        //append to coordinates array
+        coordinates += x + "#" + y + ",";
+    });
+    //on mouse up send the data
+    canvasElement.mouseup(function (event) {
+        isDrawing = false;
+        //send path coordinates only when the path is completed
+        socket.sendLine(new Line(coordinates, context));
+        coordinates = ""; //clean coordinates
+        $("#activity").html("");
+    });
+    //on clean
+    $("#clear").click(function (event) {
+        socket.sendCommand(new Command("clean"));
+        event.preventDefault();
+    });
+    //
+    //when a message is submitted
+    $("#form-new-message").submit(function (event) {
+        var messageElement = $("#message");
+        //The message will be received through the socket
+        socket.sendMessage(new Message(username, messageElement.val()));
+        messageElement.val("");
+        //it never submit the form
+        event.preventDefault();
+    });
     //Apply bindings
-    ko.applyBindings(new WhiteBoardViewModel());
+    viewModel = new WhiteBoardViewModel()
+    ko.applyBindings(viewModel);
 });
